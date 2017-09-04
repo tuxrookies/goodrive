3a28fd34eba8c8e2408fca804d495d7f4a054c5a
Pradeep Kumar <pradeepsixer@gmail.com>
2017-09-03 19:23:40 -0700

    Added Hashtable Implementation

---
+++ src/hashtable.c
@@ -0,0 +1,241 @@
+/*
+ *                ______            ____       _
+ *               / ____/___  ____  / __ \_____(_)   _____
+ *              / / __/ __ \/ __ \/ / / / ___/ / | / / _ \
+ * Project     / /_/ / /_/ / /_/ / /_/ / /  / /| |/ /  __/
+ *             \____/\____/\____/_____/_/  /_/ |___/\___/
+ *
+ * Copyright (C) 2017 Pradeep Kumar <pradeep.tux@gmail.com>
+ *
+ * This file is part of project GooDrive.
+ *
+ * GooDrive is free software: You can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * GooDrive is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with GooDrive.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include<limits.h>
+#include<stddef.h>
+#include<stdlib.h>
+#include<string.h>
+#include "hashtable.h"
+
+/*
+ * Hashtable
+ */
+struct hashtable {
+	unsigned int table_size;
+	float load_factor;
+	int (*hash_fn)(void *key);
+	int (*equals)(void *value1, void *value2);
+	unsigned int num_entries;
+	struct bucket_elem **table;
+};
+
+/*
+ * Each element in the bucket.
+ */
+struct bucket_elem {
+	struct bucket_elem *next;
+	void* key;
+	void* value;
+};
+
+/*
+ * Check whether the addition of 1 more element would cross the threshold of load_factor. If so, increase
+ * the table size for the HashTable.
+ */
+static void __check_and_resize(hashtable hashtable);
+
+/*
+ * Insert a Key-Value pair into the bucket.
+ * bucket_ptr - Pointer to the bucket.
+ * equals - A pointer to a function that checks whether the keys are equal (to update existing key, we need this).
+ * key - Key
+ * value - Value
+ */
+static void __insert_in_bucket(struct bucket_elem **bucket_ptr, int (*equals)(void*, void*), void *key, void *value);
+
+/*
+ * Compute the hash code for the input string.
+ */
+static int __hash_fn_string(void *input) {
+	char* input_str = input;
+	int hash = 0;
+	int input_len = strlen(input_str);
+	for (char* ptr = input_str; ptr < (input_str + input_len); ptr++) {
+		hash = 31 * hash + *ptr;
+	}
+	return hash;
+}
+
+/*
+ * Returns 1 if two strings - value1 and value 2 are equal, else returns 0.
+ */
+static int __equals_string(void *value1, void *value2) {
+	char *str1 = value1;
+	char *str2 = value2;
+	return strcmp(str1, str2) == 0;
+}
+
+ht_options default_ht_options() {
+	ht_options default_options = malloc(sizeof(struct hashtable_options));
+	default_options->table_size = 16;
+	default_options->load_factor = 0.75f;
+	default_options->hash_fn = &__hash_fn_string;
+	default_options->equals = &__equals_string;
+	return default_options;
+}
+
+hashtable ht_create(ht_options options) {
+	if (options == NULL) {
+		options = default_ht_options();
+	}
+	hashtable hashtable = malloc(sizeof(struct hashtable));
+	hashtable->table_size = options->table_size;
+	hashtable->load_factor = options->load_factor;
+	hashtable->hash_fn = options->hash_fn;
+	hashtable->equals = options->equals;
+	hashtable->num_entries = 0;
+
+	hashtable->table = malloc(sizeof(struct bucket_elem*) * hashtable->table_size);
+	return hashtable;
+}
+
+void* ht_put(hashtable hashtable, void *key, void *value) {
+	int hash_code = hashtable->hash_fn(key);
+	int hash_value = hash_code % hashtable->table_size;
+	__check_and_resize(hashtable);
+	void* old_value = ht_get(hashtable, key);
+	int exists_already = ht_exists(hashtable, key);
+	__insert_in_bucket(&hashtable->table[hash_value], hashtable->equals, key, value);
+	if (!exists_already) {
+		hashtable->num_entries++;
+	}
+	return old_value;
+}
+
+void* ht_get(hashtable hashtable, void *key) {
+	int hash_code = hashtable->hash_fn(key);
+	int hash_value = hash_code % hashtable->table_size;
+	struct bucket_elem *elem = hashtable->table[hash_value];
+	while (elem) {
+		if (hashtable->equals(elem->key, key)) {
+			return elem->value;
+		}
+
+		elem = elem->next;
+	}
+	return NULL;
+}
+
+void* ht_remove(hashtable hashtable, void *key) {
+	int hash_code = hashtable->hash_fn(key);
+	int hash_value = hash_code % hashtable->table_size;
+	struct bucket_elem **bucket_ptr = &hashtable->table[hash_value];
+	struct bucket_elem *elem = hashtable->table[hash_value];
+
+	while (elem) {
+		if (hashtable->equals(elem->key, key)) {
+			void *value = elem->value;
+			*bucket_ptr = elem->next;
+
+			hashtable->num_entries--;
+			return value;
+		}
+
+		bucket_ptr = &elem->next;
+		elem = elem->next;
+	}
+
+	return NULL;
+}
+
+unsigned int ht_num_entries(hashtable hashtable) {
+	return hashtable->num_entries;
+}
+
+int ht_exists(hashtable hashtable, void *key) {
+	int hash_code = hashtable->hash_fn(key);
+	int hash_value = hash_code % hashtable->table_size;
+	struct bucket_elem *elem = hashtable->table[hash_value];
+	while (elem) {
+		if (hashtable->equals(elem->key, key)) {
+			return 1;
+		}
+
+		elem = elem->next;
+	}
+	return 0;
+}
+
+static void __insert_in_bucket(struct bucket_elem **bucket_ptr,
+		int (*equals)(void*, void*), void *key, void *value) {
+	struct bucket_elem *elem = *bucket_ptr;
+
+	while (elem) {
+		/* If the key already exists, just update the value and return */
+		if (equals(elem->key, key)) {
+			elem->value = value;
+			return;
+		}
+
+		bucket_ptr = &elem->next;
+		elem = elem->next;
+	}
+
+	struct bucket_elem *new_elem = malloc(sizeof(struct bucket_elem));
+	new_elem->key = key;
+	new_elem->value = value;
+	new_elem->next = NULL;
+
+	*bucket_ptr = new_elem;
+}
+
+static void __check_and_resize(hashtable hashtable) {
+	int threshold = hashtable->table_size * hashtable->load_factor;
+
+	/* If the addition of an entry does not cross the threshold, do not increase the table size */
+	if (hashtable->num_entries + 1 <= threshold) {
+		return;
+	}
+
+	/* If the new size is less than the old size, do nothing */
+	unsigned int new_size = hashtable->table_size << 1;
+	if (new_size <= hashtable->table_size) {
+		return;
+	}
+
+	unsigned int old_size = hashtable->table_size;
+	struct bucket_elem **new_table = malloc(
+			sizeof(struct bucket_elem*) * new_size);
+	struct bucket_elem **elem_ptr = hashtable->table;
+
+	struct bucket_elem *elem, *temp;
+	int new_index;
+
+	for (; elem_ptr < hashtable->table + old_size; elem_ptr++) {
+		elem = *elem_ptr;
+		while (elem) {
+			new_index = hashtable->hash_fn(elem->key) % new_size;
+			__insert_in_bucket(new_table + new_index, hashtable->equals,
+					elem->key, elem->value);
+			temp = elem;
+			elem = elem->next;
+			free(temp);
+		}
+	}
+
+	free(hashtable->table);
+	hashtable->table = new_table;
+	hashtable->table_size = new_size;
+}
---
+++ src/hashtable.h
@@ -0,0 +1,87 @@
+/*
+ *                ______            ____       _
+ *               / ____/___  ____  / __ \_____(_)   _____
+ *              / / __/ __ \/ __ \/ / / / ___/ / | / / _ \
+ * Project     / /_/ / /_/ / /_/ / /_/ / /  / /| |/ /  __/
+ *             \____/\____/\____/_____/_/  /_/ |___/\___/
+ *
+ * Copyright (C) 2017 Pradeep Kumar <pradeep.tux@gmail.com>
+ *
+ * This file is part of project GooDrive.
+ *
+ * GooDrive is free software: You can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * GooDrive is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with GooDrive.  If not, see <http://www.gnu.org/licenses/>.
+ */
+#ifndef GOODRV_HASHTABLE_H
+#define GOODRV_HASHTABLE_H
+
+/*
+ * HashTable using Chained Hashing.
+ */
+typedef struct hashtable *hashtable;
+
+/*
+ * Options for creating the hashtable
+ * table_size - Default Table Size.
+ * load_factor - Load factor of the hash table.
+ * hash_fn - Hash function to calculate the hash code for the key.
+ * equals - pointer to a function to check whether two values are equal, or not.
+ */
+struct hashtable_options {
+	unsigned int table_size;
+	float load_factor;
+	int (*hash_fn)(void *key);
+	int (*equals)(void *value1, void *value2);
+};
+
+typedef struct hashtable_options *ht_options;
+
+/*
+ * Get the default hashtable options. The keys should be string for the default options.
+ */
+ht_options default_ht_options();
+
+/*
+ * The options for creating the hashtable.
+ */
+hashtable ht_create(ht_options options);
+
+/*
+ * Get the number of Key-Value pairs in the hashtable.
+ */
+unsigned int ht_num_entries(hashtable hashtable);
+
+/*
+ * Insert a Key-Value pair into the hashtable.
+ * If a Key-Value pair is already present, updates it to new value and returns
+ *  the previous value.
+ */
+void* ht_put(hashtable hashtable, void *key, void *value);
+
+/*
+ * Get the value for the given key.
+ */
+void* ht_get(hashtable hashtable, void *key);
+
+/*
+ * Remove the Key-Value pair from the hashtable.
+ * Returns the Value.
+ */
+void* ht_remove(hashtable hashtable, void *key);
+
+/*
+ * Check whether a key is present in the hashtable.
+ */
+int ht_exists(hashtable hashtable, void *key);
+
+#endif /* GOODRV_HASHTABLE_H */
